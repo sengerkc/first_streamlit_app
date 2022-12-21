21a0bdb11f06f5ce82137dff771063a378d4f5a0
sengerkc <sengerkc@users.noreply.github.com>
2022-12-21 08:11:47 -0500

    Update streamlit_app.py
    Stuff fruity vice json into a data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Use fruity vice data and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Stuff the fruity vice data into a dataframe
+streamlit.dataframe(fruityvice_normalized)
