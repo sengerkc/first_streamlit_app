4c9474ef78cbf2a067a488f7a0b376876ea34ae3
sengerkc <sengerkc@users.noreply.github.com>
2022-12-21 13:09:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create function 
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
    # Use fruity vice data and normalize it
    return(pandas.json_normalize(fruityvice_response.json()))
